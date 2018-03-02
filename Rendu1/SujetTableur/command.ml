174f366ba75c4c99a9a5651be8ba2eddfcdd7883
Qualitatif <quentin.de-goer-de-herve@ens-lyon.fr>
2018-03-02 11:20:58 +0100

    Better!

---
+++ Rendu1/SujetTableur/command.ml
@@ -0,0 +1,64 @@
+open Debug
+open Cell
+open Sheet
+
+(* commandes: ce que l'utilisateur peut saisir dans un fichier.
+ - La modification d'une cellule avec une nouvelle formule,
+ - l'affichage d'une cellule,
+ - l'affichage de toute la feuille *)
+type comm = Upd of cellname * form | Show of cellname | ShowAll
+let notUpToDate = ref false (* Indicates that the sheet is not up-to-date and
+that it musts be recomputed *)
+
+(************ affichage **************)
+let show_comm c =
+  match c with
+  | Upd (c,f) ->
+     begin
+       print_string (cell_name2string c);
+       print_string "=";
+       show_form f
+     end
+  | Show c ->
+     begin
+       print_string "Show(";
+       print_string (cell_name2string c);
+       print_string ")"
+     end
+  | ShowAll -> print_string "ShowAll"
+
+(************ faire tourner les commandes **************)
+
+(* exécuter une commande *)
+let run_command c = match c with
+  | Show cn ->
+    begin
+      if !notUpToDate (* checks whether run_command must recompute the sheet *)
+      then
+        (recompute_sheet() ;
+        notUpToDate := false) ; (* now the sheet is up-to-date again *)
+      let co = cellname_to_coord cn in
+      eval_p_debug (fun () ->
+          "Showing cell "
+          ^ cell_name2string cn
+        );
+      print_string (cell_val2string (read_cell co)); (* <- ici print_string, et pas p_debug, car on veut afficher au moins cela *)
+      print_newline()
+    end
+  | ShowAll ->
+    begin
+      eval_p_debug (fun () -> "Show All\n");
+      if !notUpToDate (* checks whether run_command must recompute the sheet *)
+      then
+        (recompute_sheet();
+        notUpToDate := false); (* now the sheet is up-to-date again *)
+      show_sheet ()
+    end
+  | Upd(cn,f) ->
+     let co = cellname_to_coord cn in
+     eval_p_debug (fun () -> "Update cell " ^ cell_name2string cn ^ "\n");
+     update_cell_formula co f;
+     notUpToDate := true (* the sheet is now outdated *)
+
+(* exécuter une liste de commandes *)
+let run_script cs = List.iter run_command cs
